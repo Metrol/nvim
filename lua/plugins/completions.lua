e38ee16188e1cffd0137864d5526964c026b6f39
Michael Collette <metrol@metrol.net>
2024-03-31 13:08:24 -0500

    Add an 80 column ruler

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -18,7 +18,8 @@ return {
 				snippet = {
 					-- REQUIRED - you must specify a snippet engine
 					expand = function(args)
-						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+                        -- For `luasnip` users.
+						require("luasnip").lsp_expand(args.body)
 					end,
 				},
 				window = {
@@ -30,7 +31,9 @@ return {
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                    -- Accept currently selected item. Set `select` to `false`
+                    -- to only confirm explicitly selected items. 
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
 				}),
 				sources = cmp.config.sources({
 					{ name = 'nvim_lsp' },
