64b9195a9505ceeb1bc50c77cc0874f3fed77f9e
Michael Collette <metrol@metrol.net>
2025-05-09 08:51:56 -0500

    Change out the JS LSP

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -11,7 +11,7 @@ return {
             require("mason-lspconfig").setup({
                 ensure_installed = {
                     "lua_ls",
-                    "tsserver",
+                    "ts_ls",
                     "bashls",
                     "intelephense",
                 },
@@ -34,7 +34,7 @@ return {
             lspconfig.lua_ls.setup({
                 capabilities = capabilities,
             })
-            lspconfig.tsserver.setup({
+            lspconfig.ts_ls.setup({
                 capabilities = capabilities,
             })
             lspconfig.bashls.setup({
--- lua/plugins/neo_tree.lua
+++ lua/plugins/neo_tree.lua
@@ -7,8 +7,9 @@ return {
       "MunifTanjim/nui.nvim"
     },
     config = function()
-        vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
---         vim.keymap.set('n', '<leader>n', ':Neotree toggle<CR>')
+--        vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
+        vim.keymap.set('n', '<leader>n', ':Neotree toggle<CR>')
+--        vim.keymap.set('n', '<C-n>', ':Neotree toggle<CR>')
         require("neo-tree").setup({
 --             Automatically close tree view after file selected
 --            event_handlers = {
