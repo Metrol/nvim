5ab8f92a1f483d1021bf5250fd67fa022521f618
Michael Collette <metrol@metrol.net>
2025-06-27 15:31:52 -0500

    Give top level leader key items proper names
    Fancy submenu is what this amounts to.  Should make it easier for me to
    remember what is where.

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,18 +1,25 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+        spec = {
+            {
+                mode = { "n", "v" },
+                { "<leader>d", group = "Debug" },
+                { "<leader>D", group = "Database" },
+                { "<leader>f", group = "Finders" },
+                { "<leader>g", group = "LSP Functions" },
+                { "<leader>w", group = "Windowing" },
+            }
+        }
     },
-  },
+    keys = {
+        {
+            "<leader>?",
+            function()
+                require("which-key").show({ global = false })
+            end,
+            desc = "Buffer Local Keymaps (which-key)",
+        },
+    },
 }
