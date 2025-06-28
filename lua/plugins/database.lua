0f648067525b67fe3ec06a5c1521fba686217877
Michael Collette <metrol@metrol.net>
2025-06-27 22:34:20 -0500

    Connect the PostgreSQL LSP to cmp

--- lua/plugins/database.lua
+++ lua/plugins/database.lua
@@ -1,10 +1,10 @@
 return {
-  "tpope/vim-dadbod",
-  "kristijanhusak/vim-dadbod-completion",
-  "kristijanhusak/vim-dadbod-ui",
+    "tpope/vim-dadbod",
+    "kristijanhusak/vim-dadbod-completion",
+    "kristijanhusak/vim-dadbod-ui",
 
-  vim.keymap.set('n', '<leader>Do', function() vim.cmd [[DBUI]] end, { desc = 'Open Database Manage' }),
-  vim.keymap.set('n', '<leader>Dt', function() vim.cmd [[DBUIToggle]] end, { desc = 'Toggle Database Manage' }),
-  vim.keymap.set('n', '<leader>Db', function() vim.cmd [[DBUIFindBuffer]] end, { desc = 'Connect Buffer to DB' }),
-  vim.keymap.set('n', '<leader>Dq', function() vim.cmd [[DBUIClose]] end, { desc = 'Close Database Manage' })
+    vim.keymap.set('n', '<leader>Do', function() vim.cmd [[DBUI]] end, { desc = 'Open Database Manage' }),
+    vim.keymap.set('n', '<leader>Dt', function() vim.cmd [[DBUIToggle]] end, { desc = 'Toggle Database Manage' }),
+    vim.keymap.set('n', '<leader>Db', function() vim.cmd [[DBUIFindBuffer]] end, { desc = 'Connect Buffer to DB' }),
+    vim.keymap.set('n', '<leader>Dq', function() vim.cmd [[DBUIClose]] end, { desc = 'Close Database Manage' })
 }
