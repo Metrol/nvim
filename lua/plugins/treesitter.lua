f1abcf2ada7a0aaad3fc9f8ab187ca41491b4aaf
Michael Collette <metrol@metrol.net>
2025-07-06 16:12:19 -0500

    Bring in treesitter text objects and custom selectors

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,20 +1,69 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local config = require("nvim-treesitter.configs")
-        config.setup({
-            auto_install = true,
-            ensure_installed = {"php", "javascript"},
-            sync_install = false,
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            local config = require("nvim-treesitter.configs")
+            config.setup({
+                auto_install = true,
+                ensure_installed = {"php", "javascript"},
+                sync_install = false,
 
-            highlight = {
-                enable = true
-            },
-            indent = {
-                enable = true
-            }
-        })
-    end
+                highlight = {
+                    enable = true
+                },
+
+                indent = {
+                    enable = true
+                },
+
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true,
+                        keymaps = {
+                            -- You can use the capture groups defined in textobjects.scm
+                            ["af"] = {
+                                query = "@function.outer",
+                                desc  = "Around function"
+                            },
+                            ["if"] = {
+                                query = "@function.inner",
+                                desc  = "Inner part of function"
+                            },
+                            ["ii"] = {
+                                query = "@conditional.inner",
+                                desc = "Inner part of conditional"
+                            },
+                            ["ai"] = {
+                                query = "@conditional.outer",
+                                desc = "Around conditional"
+                            },
+                            ["il"] = {
+                                query = "@loop.inner",
+                                desc = "Inner part of loop"
+                            },
+                            ["al"] = {
+                                query = "@loop.outer",
+                                desc = "Around loop"
+                            },
+                            ["ia"] = {
+                                query = "@parameter.inner",
+                                desc = "Parameter/Argument"
+                            },
+                        },
+                        selection_modes = {
+                            ['@parameter.outer'] = 'v', -- charwise
+                            ['@function.outer'] = 'v', -- linewise
+                            ['@class.outer'] = '<c-v>', -- blockwise
+                        },
+                        include_surrounding_whitespace = true,
+                    }
+                }
+            })
+        end
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects"
+    }
 }
-
