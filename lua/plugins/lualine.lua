dc144072ae93a4b36ceabe3bf3c7438fe001c74b
Michael Collette <metrol@metrol.net>
2025-07-08 14:25:35 -0500

    Remove the gruvbox theme from lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,7 +4,6 @@ return {
     config = function()
         require('lualine').setup({
             options = {
-                -- theme = 'gruvbox'
                 theme = 'catppuccin'
             },
             sections = {
