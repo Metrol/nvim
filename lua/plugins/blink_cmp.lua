8de0d71315658099c3939bfd0350ffd2d2b80426
Michael Collette <metrol@metrol.net>
2025-07-28 12:48:46 -0500

    Fix the autocomplete for PostgreSQL queries

--- lua/plugins/blink_cmp.lua
+++ lua/plugins/blink_cmp.lua
@@ -30,7 +30,15 @@ return {
         -- elsewhere in your config, without redefining it, due to `opts_extend`
         sources = {
             default = { 'lsp', 'path', 'snippets', 'buffer' },
+            per_filetype = {
+                sql = { 'dadbod', 'buffer' }
+            },
+            -- add vim-dadbod-completion to your completion providers
+            providers = {
+                dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+            },
         },
+
 
         -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
         -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
