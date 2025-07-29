0a87046a45290e9c9dadfe187977b17933824869
Michael Collette <metrol@metrol.net>
2025-07-29 15:09:22 -0500

    Turn off actions that don't work for me
    Also, reformat the indenting on this thing

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -4,15 +4,15 @@ return {
         dependencies = { 'saghen/blink.cmp' },
         config = function()
 
-        local get_intelephense_license = function()
-            local f = assert(io.open(os.getenv("HOME") .. "/.config/intelephense/license.txt", "rb"))
-            local content = f:read("*a")
-            f:close()
-            return string.gsub(content, "%s+", "")
-        end
+            local get_intelephense_license = function()
+                local f = assert(io.open(os.getenv("HOME") .. "/.config/intelephense/license.txt", "rb"))
+                local content = f:read("*a")
+                f:close()
+                return string.gsub(content, "%s+", "")
+            end
 
-        local lspconfig = require("lspconfig")
-	    local capabilities = require('blink.cmp').get_lsp_capabilities()
+            local lspconfig = require("lspconfig")
+            local capabilities = require('blink.cmp').get_lsp_capabilities()
 
             lspconfig.lua_ls.setup({ capabilities = capabilities })
             lspconfig.ts_ls.setup({ capabilities = capabilities })
@@ -28,14 +28,14 @@ return {
             vim.api.nvim_create_autocmd("LspAttach", {
                 desc = "use lsp folding",
                 callback = function(event)
-                local client = assert(vim.lsp.get_client_by_id(event.data.client_id))
+                    local client = assert(vim.lsp.get_client_by_id(event.data.client_id))
 
-                if not client:supports_method("textDocument/foldingRange") then
-                    return
-                end
+                    if not client:supports_method("textDocument/foldingRange") then
+                        return
+                    end
 
-                local win = vim.api.nvim_get_current_win()
-                vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
+                    local win = vim.api.nvim_get_current_win()
+                    vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
                 end,
             })
 
@@ -43,14 +43,14 @@ return {
             vim.keymap.set('n', '<leader>gk', vim.lsp.buf.hover, { desc = "Show Description" })
             vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Find Uses" })
             vim.keymap.set('n', '<space>ga', vim.lsp.buf.code_action, { desc = "Code Action" })
+            vim.keymap.set('n', '<leader>gc', ':LspRestart<CR>', { desc = "Restart LSP Servers" }) -- Nope
             vim.keymap.set({ "n", "v" }, "<leader>ga", vim.lsp.buf.code_action, { desc = "Code Action" })
 
             -- Commands that don't seem to do anything with intelephense
-            vim.keymap.set('n', '<leader>gD', vim.lsp.buf.declaration, { desc = "Declaration" }) -- doesn't work
-            vim.keymap.set('n', '<leader>gi', vim.lsp.buf.implementation, { desc = "Implementation" }) -- doesn't work
-            vim.keymap.set('n', '<leader>gK', vim.lsp.buf.signature_help, { desc = "Signature Help" }) -- doesn't work
-            vim.keymap.set('n', '<leader>gt', vim.lsp.buf.type_definition, { desc = "Type Definition" }) -- Nope
-            vim.keymap.set('n', '<leader>gc', ':LspRestart<CR>', { desc = "Restart LSP Servers" }) -- Nope
+            -- vim.keymap.set('n', '<leader>gD', vim.lsp.buf.declaration, { desc = "Declaration" }) -- doesn't work
+            -- vim.keymap.set('n', '<leader>gi', vim.lsp.buf.implementation, { desc = "Implementation" }) -- doesn't work
+            -- vim.keymap.set('n', '<leader>gK', vim.lsp.buf.signature_help, { desc = "Signature Help" }) -- doesn't work
+            -- vim.keymap.set('n', '<leader>gt', vim.lsp.buf.type_definition, { desc = "Type Definition" }) -- Nope
             -- vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {}) -- Don't trust it
         end,
     },
