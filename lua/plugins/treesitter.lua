ec953f8dd84ce2ca9267a47dee6620b2a213b713
Michael Collette <metrol@metrol.net>
2025-11-06 08:53:13 -0600

    Let treesitter handle indenting
    This seems to have fixed my problems with indenting JavaScript doc
    blocks.  More testing is needed, but so far it looks pretty good.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -14,7 +14,7 @@ return {
                 },
 
                 indent = {
-                    enable = false
+                    enable = true
                 },
                 modules = {},
                 ignore_install = {},
