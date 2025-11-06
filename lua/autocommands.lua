ea4ecb478c00cecb5ba4935af6cc55f2ff0c82dd
Michael Collette <metrol@metrol.net>
2025-11-06 08:54:08 -0600

    No longer need to force a neotree refresh
    The changes I made to the neotree config seem to handle updating the
    file tree quite nicely without having to force it to happen.

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -12,15 +12,15 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 -- Refreshes the NeoTree file listing after LazyGit closes
-vim.api.nvim_create_autocmd({ "BufLeave" }, {
-	pattern = { "*lazygit*" },
-	group = vim.api.nvim_create_augroup("git_refresh_neotree", {clear = true}),
-	callback = function()
-		require("neo-tree.sources.filesystem.commands").refresh(
-			require("neo-tree.sources.manager").get_state("filesystem")
-		)
-	end,
-})
+-- vim.api.nvim_create_autocmd({ "BufLeave" }, {
+-- 	pattern = { "*lazygit*" },
+-- 	group = vim.api.nvim_create_augroup("git_refresh_neotree", {clear = true}),
+-- 	callback = function()
+-- 		require("neo-tree.sources.filesystem.commands").refresh(
+-- 			require("neo-tree.sources.manager").get_state("filesystem")
+-- 		)
+-- 	end,
+-- })
 
 -- Set the mode to "normal" after telescope has selected a file
 vim.api.nvim_create_autocmd({ "BufLeave", "BufWinLeave" }, {
