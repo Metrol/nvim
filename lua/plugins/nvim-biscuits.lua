eb3db4a7e5b5a790ce177b65ca1d4b5355447d0d
Michael Collette <metrol@metrol.net>
2025-11-09 13:01:23 -0600

    Trying out biscuits
    It mostly works, but in a few places it doesn't.  May not keep it due to
    some of the visual noise and slow downs while scrolling.  That, or I may
    keep it around, but disabled by default.

---
+++ lua/plugins/nvim-biscuits.lua
@@ -0,0 +1,40 @@
+return {
+    'code-biscuits/nvim-biscuits',
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter',
+    },
+    opts = {
+        -- Config goes here
+    },
+    config = function()
+        require('nvim-biscuits').setup({
+            cursor_line_only = true,
+            toggle_keybind = "<leader>sb",
+            default_config = {
+                max_length = 12,
+                min_distance = 5,
+                prefix_string = " 📎 "
+            },
+            language_config = {
+                html = {
+                    prefix_string = " 🌐 "
+                },
+                twig = {
+                    prefix_string = " 🌐 "
+                },
+                javascript = {
+                    prefix_string = " ✨ ",
+                    max_length = 80
+                },
+                php = {
+                    prefix_string = " ✨ ",
+                    max_length = 80
+                },
+                python = {
+                    disabled = true
+                }
+            }
+        })
+    end
+}
+
