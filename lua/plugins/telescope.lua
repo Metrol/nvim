605d8611800cb588dba223a100e440747819979c
Michael Collette <metrol@metrol.net>
2026-02-26 16:48:40 -0600

    Change the shortcut for the Telescope buffer find

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,7 +7,7 @@ return {
             local builtin = require("telescope.builtin")
             -- vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find Files'})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live GREP' })
-            vim.keymap.set('n', '<leader>wf', builtin.buffers, { desc = 'Search Buffers' })
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Search Buffers' })
             vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Help Tags' })
             -- Outline View
             vim.keymap.set('n', '<leader>fo', builtin.treesitter, { desc = 'Tree Sitter' })
