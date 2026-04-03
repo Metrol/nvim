4ae0d5d2f4c468ebae5224eca1f838f6974fb880
Michael Collette <metrol@metrol.net>
2026-04-03 00:34:58 -0500

    Remove treesitter for now
    Generating all kinds of errors for me when doing some basic editing.
    More research is likely needed to figure out why.

--- lua/inw/treesitter.lua
+++ lua/inw/treesitter.lua
@@ -6,7 +6,7 @@ return {
             local config = require("nvim-treesitter.configs")
             config.setup({
                 auto_install = true,
-                ensure_installed = {"php", "phpdoc", "twig", "javascript", "jsdoc"},
+                ensure_installed = {"php", "phpdoc", "twig", "javascript", "jsdoc", "html"},
                 sync_install = false,
 
                 highlight = {
