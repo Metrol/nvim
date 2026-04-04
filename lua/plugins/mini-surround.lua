71992445ddf23bbec38b99194231d8b2913ceb1f
Michael Collette <metrol@metrol.net>
2026-04-04 13:10:45 -0500

    Changing up the key maps for surround and flash

--- lua/plugins/mini-surround.lua
+++ lua/plugins/mini-surround.lua
@@ -5,12 +5,12 @@ return {
     config = function()
         require("mini.surround").setup({
             mappings = {
-                add       = 'gsa', -- Add surrounding in Normal and Visual modes
-                delete    = 'gsd', -- Delete surrounding
-                find      = 'gsf', -- Find surrounding (to the right)
-                find_left = 'gsF', -- Find surrounding (to the left)
-                highlight = 'gsh', -- Highlight surrounding
-                replace   = 'gsr', -- Replace surrounding
+                add       = 'sa', -- Add surrounding in Normal and Visual modes
+                delete    = 'sd', -- Delete surrounding
+                find      = 'sf', -- Find surrounding (to the right)
+                find_left = 'sF', -- Find surrounding (to the left)
+                highlight = 'sh', -- Highlight surrounding
+                replace   = 'sr', -- Replace surrounding
 
                 suffix_last = 'l', -- Suffix to search with "prev" method
                 suffix_next = 'n', -- Suffix to search with "next" method
