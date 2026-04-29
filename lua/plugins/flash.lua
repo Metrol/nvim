c426d99f667eb432df463ebbc50cdadaa53df087
Michael Collette <metrol@metrol.net>
2026-04-29 10:08:42 -0500

    Moving the flash keymap to just leader-j

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -8,6 +8,7 @@
 --
 -- For all the fancy things this plugin does, I only really ever use the flash
 -- jump.  For that reason, the other options have been disabled.
+--
 return {
     "folke/flash.nvim",
     event = "VeryLazy",
@@ -15,8 +16,8 @@ return {
     opts = {},
     -- stylua: ignore
     keys = {
-        { "<leader>j", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-        -- { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+        { "<leader>j", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash Jump" },
+        -- { "<leader>J", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
         -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
         -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
         -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
