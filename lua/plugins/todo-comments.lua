db16e64242ab929e7f3d21b2e2e6760da57d4e50
Michael Collette <metrol@metrol.net>
2026-07-28 11:52:32 -0500

    New plugin for TODO comments
    May need to work in some shortcuts for this later.  Seems to work really
    nicely.  Only problem is that I've got a lot of JS libs that use those
    same keywords.

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,17 @@
+--
+-- Todo Comments
+--
+-- todo-comments is a lua plugin for Neovim >= 0.8.0 to highlight and
+-- search for todo comments like TODO, HACK, BUG in your code base.
+--
+-- https://github.com/folke/todo-comments.nvim
+--
+return {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+    }
+}
